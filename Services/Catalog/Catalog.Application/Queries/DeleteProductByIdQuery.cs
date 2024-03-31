9065529fe4571692e363ee28d3c57bc3d4b1f0d8
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2024-03-31 16:08:47 +0100

    Define Queries

---
+++ Services/Catalog/Catalog.Application/Queries/DeleteProductByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Queries
+{
+    public class DeleteProductByIdQuery : IRequest<bool>
+    {
+        public string Id { get; set; }
+
+        public DeleteProductByIdQuery(string id)
+        {
+            Id = id;
+        }
+    }
+}
