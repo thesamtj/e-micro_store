3f5947e346382b3495ad7fa807eb92072274ee77
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2024-04-10 14:05:49 +0100

    Add items to App

---
+++ Services/Ordering/Ordering.Application/Extensions/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviour;
+using Ordering.Application.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Extensions
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CheckoutOrderCommandHandler).Assembly));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            return services;
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Application/Handlers/GetOrderListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
+using Ordering.Core.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Handlers
+{
+    public class GetOrderListQueryHandler : IRequestHandler<GetOrderListQuery, List<OrderResponse>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrderListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<OrderResponse>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrderResponse>>(orderList);
+        }
+    }
+}
