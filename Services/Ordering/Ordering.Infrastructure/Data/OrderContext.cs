23c5de48f1bd990cfa5a197b394acdcf83e03bf6
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2024-04-10 13:19:51 +0100

    Add items to Infrastructure

---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Common;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "sam"; //TODO: This will be replaced Identity Server
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "sam"; //TODO: This will be replaced Identity Server
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation($"Ordering Database: {typeof(OrderContext).Name} seeded.");
+            }
+        }
+
+        private static IEnumerable<Order> GetOrders()
+        {
+            return new List<Order>
+            {
+                new()
+                {
+                    UserName = "sam",
+                    FirstName = "Samuel",
+                    LastName = "Tijani",
+                    EmailAddress = "omalitijanisam@gmail.com",
+                    AddressLine = "Abuja",
+                    Country = "Nigeria",
+                    TotalPrice = 750,
+                    State = "AB",
+                    ZipCode = "560001",
+
+                    CardName = "Visa",
+                    CardNumber = "1234567890123456",
+                    CreatedBy = "Samuel",
+                    Expiration = "12/25",
+                    Cvv = "123",
+                    PaymentMethod = 1,
+                    LastModifiedBy = "Samuel",
+                    LastModifiedDate = new DateTime(),
+                }
+            };
+        }
+    }
+}
