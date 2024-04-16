66b396d7a90269937a8c10fcf1aa57eac6f212c4
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2024-04-16 12:59:33 +0100

    Define the events

---
+++ Infrastructure/EventBus.Messages/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class BaseIntegrationEvent
+    {
+        //CO-Relation Id
+        public string CorrelationId { get; set; }
+        public DateTime CreationDate { get; private set; }
+
+        public BaseIntegrationEvent()
+        {
+            CorrelationId = Guid.NewGuid().ToString();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public BaseIntegrationEvent(Guid correlationId, DateTime creationDate)
+        {
+            CorrelationId = correlationId.ToString();
+            CreationDate = creationDate;
+        }
+    }
+}
