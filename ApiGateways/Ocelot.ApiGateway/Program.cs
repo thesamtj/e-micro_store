b0ca3c8a87f543d903e63051094310f5be73768c
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2024-04-17 14:46:11 +0100

    Configure ocelot program

--- ApiGateways/Ocelot.ApiGateway/Program.cs
+++ ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -1,44 +1,40 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var env = builder.Environment;
 
 // Add services to the container.
+builder.Configuration.AddJsonFile($"ocelot.{env.EnvironmentName}.json", true, true);
+services.AddOcelot()
+            .AddCacheManager(o => o.WithDictionaryHandle());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 var app = builder.Build();
+var env2 = app.Environment;
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseRouting();
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/", async context =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    await context.Response.WriteAsync(
+        "Hello Ocelot");
+});
 
-app.Run();
+await app.UseOcelot();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+//app.Run();
