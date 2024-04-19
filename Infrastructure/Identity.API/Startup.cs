398e8d45465c5e0bd6d6cff000162c1e8eda4d7e
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2024-04-19 02:19:20 +0100

    Fix errors

--- Infrastructure/Identity.API/Startup.cs
+++ Infrastructure/Identity.API/Startup.cs
@@ -25,7 +25,7 @@ namespace Identity.API
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             var builder = services.AddIdentityServer(options =>
             {
